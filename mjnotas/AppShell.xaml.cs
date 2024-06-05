fd5ea051bf1b897f0e5ff636b305a382f1e6da04
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-06-05 12:17:10 -0500

    Arreglo de cosas de front en MJContarpage

--- mjnotas/AppShell.xaml.cs
+++ mjnotas/AppShell.xaml.cs
@@ -6,8 +6,25 @@
         {
             InitializeComponent();
 
-            Routing.RegisterRoute(nameof(Views.MJnotepage), typeof(Views.MJnotepage));
+            
+            Routing.RegisterRoute(nameof(Views.MJAllNotesPage), typeof(Views.MJAllNotesPage));
             Routing.RegisterRoute(nameof(Views.MJContarpage), typeof(Views.MJContarpage));
+            Routing.RegisterRoute(nameof(Views.MJaboutpage), typeof(Views.MJaboutpage));
+        }
+
+        private async void OnNotesTapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//notes");
+        }
+
+        private async void OnCountTapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//count");
+        }
+
+        private async void OnAboutTapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//about");
         }
     }
 }
